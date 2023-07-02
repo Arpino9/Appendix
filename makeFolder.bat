a1c8792ca442ec65b8c18a984ded7186b8f92d59
アルピーノ <77906739+Arpino9@users.noreply.github.com>
2023-07-02 10:32:08 +0900

    Update makeFolder.bat

--- makeFolder.bat
+++ makeFolder.bat
@@ -2,7 +2,8 @@
 setlocal
 
 rem 作成先のフォルダ名と生成するフォルダ名を定義する
-rem 以下は「デスクトップ > 作業フォルダ > m月」のディレクトリ構成の場合、当月内のフォルダに「yyyy/MM/DD」の形式でフォルダを作成している
+rem 以下は「デスクトップ ＞ 作業フォルダ ＞ m月」のディレクトリ構成の場合、当月内のフォルダに「yyyy/MM/DD」の形式でフォルダを作成している
+rem 文字化けする場合は、コマンドプロンプトから「chcp」で検索。932ならShift-JISなので、バッチファイルの文字コードもANSIにする。
 set Month=%date:~5,2%
 set FOLDER=%userprofile%\Desktop\作業フォルダ\%Month%月\
 set FILE_NAME=%date:~0,4%%date:~5,2%%date:~8,2%
