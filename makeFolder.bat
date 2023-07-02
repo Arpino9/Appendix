7ca90b31fff48c75149983c6a4bef057fa5835cd
アルピーノ <77906739+Arpino9@users.noreply.github.com>
2023-07-02 10:29:00 +0900

    Update makeFolder.bat

--- makeFolder.bat
+++ makeFolder.bat
@@ -3,7 +3,7 @@ setlocal
 
 rem 作成先のフォルダ名と生成するフォルダ名を定義する
 set Month=%date:~5,2%
-set FOLDER=%userprofile%\Desktop\動画編集\%Month%月\
+set FOLDER=%userprofile%\Desktop\作業フォルダ\%Month%月\
 set FILE_NAME=%date:~0,4%%date:~5,2%%date:~8,2%
 
 mkdir %FOLDER%%FILE_NAME%
